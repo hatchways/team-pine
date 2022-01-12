e22a969274f4b2143f6b2b36728a909acc25ddf0
Roland M <rj1337@gmail.com>
2022-01-13 06:40:43 +0900

    made demo user button login and placed on both register and login page

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,11 +1,13 @@
 import { FormikHelpers } from 'formik';
 import register from '../../helpers/APICalls/register';
+import login from '../../helpers/APICalls/login';
 import SignUpForm from './SignUpForm/SignUpForm';
 import { useAuth } from '../../context/useAuthContext';
 import { useSnackBar } from '../../context/useSnackbarContext';
 import AuthPageWrapper from '../../components/AuthPageWrapper/AuthPageWrapper';
 import PageContainer from '../../components/PageContainer/PageContainer';
 import AuthPageFooter from '../../components/AuthPageFooter/AuthPageFooter';
+import DemoLogin from '../Login/DemoLogin/DemoLogin';
 
 export default function Register(): JSX.Element {
   const { updateLoginContext } = useAuth();
@@ -32,10 +34,31 @@ export default function Register(): JSX.Element {
     });
   };
 
+  const handleSubmitDemo = (
+    { email, password }: { email: string; password: string },
+    { setSubmitting }: FormikHelpers<{ email: string; password: string }>,
+  ) => {
+    login(email, password).then((data) => {
+      if (data.error) {
+        setSubmitting(false);
+        updateSnackBarMessage(data.error.message);
+      } else if (data.success) {
+        updateLoginContext(data.success);
+      } else {
+        // should not get here from backend but this catch is for an unknown issue
+        console.error({ data });
+
+        setSubmitting(false);
+        updateSnackBarMessage('An unexpected error occurred. Please try again');
+      }
+    });
+  };
+
   return (
     <PageContainer>
       <AuthPageWrapper header="Sign up">
         <SignUpForm handleSubmit={handleSubmit} />
+        <DemoLogin handleSubmit={handleSubmitDemo} />
         <AuthPageFooter text="Already a member?" anchorText="Login" anchorTo="/login" />
       </AuthPageWrapper>
     </PageContainer>
