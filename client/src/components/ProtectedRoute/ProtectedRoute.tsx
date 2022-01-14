9b92a6d5329891fb4dbe732493670775c9e1e47d
Gurkirt Singh <GurkirtSingh143@gmail.com>
2022-01-13 22:30:51 -0800

    created 'protected route' function component

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { useAuth } from '../../context/useAuthContext';
+import { useSocket } from '../../context/useSocketContext';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Route, Redirect, RouteProps, RouteComponentProps } from 'react-router-dom';
+
+const ProtectedRoute: React.FC<RouteProps> = ({ component, render, ...routeProps }) => {
+  const { loggedInUser } = useAuth();
+  const { initSocket } = useSocket();
+
+  useEffect(() => {
+    initSocket();
+  }, [initSocket]);
+
+  if (loggedInUser === undefined) {
+    // show progress circule until we have some value for user
+    return <CircularProgress />;
+  }
+  if (!loggedInUser) {
+    // redirect if user is not authenticated
+    return (
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: {
+            from: routeProps.location,
+          },
+        }}
+      />
+    );
+  }
+
+  return (
+    <Route
+      {...routeProps}
+      render={(props: RouteComponentProps) => {
+        if (loggedInUser) {
+          // render protected component on valid user
+          if (component) {
+            return React.createElement(component, props);
+          }
+          if (render) {
+            return render(props);
+          }
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
