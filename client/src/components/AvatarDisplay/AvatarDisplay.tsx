55f690b2d3452dfcef521e50efe02519683312e0
Gurkirt Singh <GurkirtSingh143@gmail.com>
2022-01-16 09:25:16 -0800

    fixed 'Avtar display' component
    Changes made to display user's profile picture
    
    Before 'Avtar display' component was displaying random picture from a website

--- client/src/components/AvatarDisplay/AvatarDisplay.tsx
+++ client/src/components/AvatarDisplay/AvatarDisplay.tsx
@@ -4,10 +4,21 @@ import { User } from '../../interface/User';
 interface Props {
   loggedIn: boolean;
   user: User;
+  photoUrl?: string;
+  width?: number;
+  height?: number;
 }
 
-const AvatarDisplay = ({ user }: Props): JSX.Element => {
-  return <Avatar alt="Profile Image" src={`https://robohash.org/${user.email}.png`} />;
+const AvatarDisplay = ({ user, width, height, photoUrl }: Props): JSX.Element => {
+  if (photoUrl !== '') {
+    return <Avatar sx={{ width, height }} alt="Profile Image" src={photoUrl} />;
+  } else {
+    return (
+      <Avatar sx={{ width, height }} alt="Profile Image">
+        {user.name[0]}
+      </Avatar>
+    );
+  }
 };
 
 export default AvatarDisplay;
