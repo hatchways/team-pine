df0b827d9240e6d9920297e2494794fb18a45acc
rj1337 <mathesonroland@gmail.com>
2022-01-16 15:09:15 +0900

    Made requested changes

--- server/controllers/notification.js
+++ server/controllers/notification.js
@@ -2,69 +2,82 @@ const Notification = require("../models/Notification");
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 
-// @route PUT /profile/edit
-// @desc edit user profile
+// @route POST /notification/
+// @desc create a new notification
 // @access Public
 
 exports.createNotification = asyncHandler(async (req, res, next) => {
-const userId = await User.findById(req.user.id);
-const { type, title, description } = req.body;
-const notification = await Notification.create({userId, type, title, description});
+  const user = await User.findById(req.user.id);
+  const { type, title, description } = req.body;
+  const notification = await Notification.create({
+    user,
+    type,
+    title,
+    description,
+  });
 
   res.status(200).json({
     success: {
-      notification: notification
+      notification: notification,
     },
   });
-
 });
 
+// @route PUT /notification/mark-read/:notificationId
+// @desc changes mark as read property to true on a notification
+// @access Private
 
 exports.markAsRead = asyncHandler(async (req, res, next) => {
-const {notificationId} = req.params;
-const update = { read: true };
-const foundNotification = await Notification.findOneAndUpdate(notificationId, update);
-const readNotification = await foundNotification.save()
+  const { notificationId } = req.params;
+  const foundNotification = await Notification.findById(notificationId);
+
+  if (foundNotification.user.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("Forbidden");
+  }
+
+  foundNotification.read = true;
+
+  await foundNotification.save();
 
   res.status(200).json({
     success: {
-      notification: readNotification
+      notification: foundNotification,
     },
   });
-
 });
+
+// @route POST /notification/all
+// @desc Retrieves all notifications for a user
+// @access Private
 
 exports.retrieveUsersNotifications = asyncHandler(async (req, res, next) => {
-const userId = req.user.id;
-const foundNotifications = await Notification.find({ userId: userId })
+  const userId = req.user.id;
+  const foundNotifications = await Notification.find({ user: userId });
 
-    if (!foundNotifications) {
-    res.status(404);
-    throw new Error("No notifications found");
-  }
-
-    res.status(200).json({
+  res.status(200).json({
     success: {
       notifications: foundNotifications,
     },
   });
-
 });
 
-exports.retrieveUsersUnreadNotifications = asyncHandler(async (req, res, next) => {
-const userId = req.user.id;
-const foundNotifications =  await Notification.find({ userId: userId, read: false })
+// @route POST /notification/unread
+// @desc Retrieves all unread notifications for a user
+// @access Private
 
-    if (!foundNotifications) {
-    res.status(404);
-    throw new Error("No notifications found");
-  }
+exports.retrieveUsersUnreadNotifications = asyncHandler(
+  async (req, res, next) => {
+    const userId = req.user.id;
+    const foundNotifications = await Notification.find({
+      user: userId,
+      read: false,
+    });
 
     res.status(200).json({
-    success: {
-      notifications: foundNotifications,
-    },
-  });
-
-
-});
+      success: {
+        notifications: foundNotifications,
+      },
+    });
+  }
+);
--- server/models/Notification.js
+++ server/models/Notification.js
@@ -1,32 +1,38 @@
 const mongoose = require("mongoose");
 
-const notificationSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
+const notificationSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      enum: ["request", "payment", "message"],
+      lowercase: true,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
   },
-  type: {
-    type: String,
-    enum: ['request', 'payment', 'message'],
-    lowercase: true,
-    required: true
-  },
-  title: {
-    type: String,
-    default: "",
-    required: true
-  },
-  description: {
-    type: String,
-    default: "",
-        required: true
-  },
-  read: {
-    type: Boolean,
-    default: false,
-    required: true
-}
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-module.exports = Notification = mongoose.model("Notification", notificationSchema);
+module.exports = Notification = mongoose.model(
+  "Notification",
+  notificationSchema
+);
