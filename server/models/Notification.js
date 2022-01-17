a5b8e34025fb86025efd44315c25036a1964e15f
Roland M <rj1337@gmail.com>
2022-01-18 07:46:05 +0900

    Add receiver and sender properties to notification model

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
   {
-    user: {
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    sender: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: "User",
