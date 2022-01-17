b2482e4ffb0a20f91513a7a17c8d79503f01fb3a
Gurkirt Singh <GurkirtSingh143@gmail.com>
2022-01-17 13:21:32 -0800

    Fixed: Protected Route Component
    Extended Route Props to change component filed from optional to required
    Removed Socket updates.
    Simplified return statement.

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,51 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useAuth } from '../../context/useAuthContext';
-import { useSocket } from '../../context/useSocketContext';
 import CircularProgress from '@mui/material/CircularProgress';
-import { Route, Redirect, RouteProps, RouteComponentProps } from 'react-router-dom';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
 
-const ProtectedRoute = ({ component: Component, ...rest }: RouteProps) : JSX.Element  => {
+interface ProtectedRouteProps extends RouteProps {
+  component: React.ComponentType<any>;
+}
+const ProtectedRoute = ({ component: Component, ...rest }: ProtectedRouteProps): JSX.Element => {
   const { loggedInUser } = useAuth();
-  const { initSocket } = useSocket();
-
-  useEffect(() => {
-    initSocket();
-  }, [initSocket]);
 
   if (loggedInUser === undefined) {
     // show progress circule until we have some value for user
     return <CircularProgress />;
   }
-  if (!loggedInUser) {
-    // redirect if user is not authenticated
-    return (
-      <Redirect
-        to={{
-          pathname: '/login',
-          state: {
-            from: routeProps.location,
-          },
-        }}
-      />
-    );
-  }
 
-  return (
-    return <Route {... rest} render={(props) => (loggedInUser ? <Component {...props} /> : <Redirect to="/login" />)} />;
-      {...routeProps}
-      render={(props: RouteComponentProps) => {
-        if (loggedInUser) {
-          // render protected component on valid user
-          if (component) {
-            return React.createElement(component, props);
-          }
-          if (render) {
-            return render(props);
-          }
-        }
-      }}
-    />
-  );
+  return <Route {...rest} render={(props) => (loggedInUser ? <Component {...props} /> : <Redirect to="/login" />)} />;
 };
 
 export default ProtectedRoute;
