90ccba700ace4b6865a4aff636a50e281badbc07
Roland M <rj1337@gmail.com>
2022-01-18 10:13:37 +0900

    styling changes

--- client/src/pages/Profiles/ProfileListings/useStyles.ts
+++ client/src/pages/Profiles/ProfileListings/useStyles.ts
@@ -2,7 +2,29 @@ import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  profileCard: {},
+  profileCard: {
+    maxWidth: '350px',
+    margin: '30px',
+    textAlign: 'center',
+    display: 'flex',
+  },
+  header: {
+    gutterBottom: true,
+    textAlign: 'center',
+  },
+  profileAvatar: {
+    margin: '30px',
+    justifyContent: 'center',
+  },
+  profileName: {},
+  profileTitle: {},
+  profileRating: {},
+  profileDescription: {},
+  profileAddress: { textAlign: 'left' },
+  profilePay: { fontWeight: 'bolder', textAlign: 'right' },
+  button: {
+    textAlign: 'center',
+  },
 }));
 
 export default useStyles;
