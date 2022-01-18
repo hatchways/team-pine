829342a5b811a2b033bdf4e677f13267462cb649
Roland M <rj1337@gmail.com>
2022-01-18 10:13:23 +0900

    styling changes

--- client/src/pages/Profiles/ProfileListings/ProfileListing.tsx
+++ client/src/pages/Profiles/ProfileListings/ProfileListing.tsx
@@ -5,19 +5,17 @@ import {
   CardMedia,
   CardActionArea,
   Grid,
-  ListItemText,
-  Menu,
-  ListItem,
-  List,
+  Rating,
+  Box,
   Avatar,
   Typography,
-  ListItemAvatar,
-  Badge,
+  Divider,
   Button,
 } from '@mui/material';
 import useStyles from './useStyles';
 import PageContainer from '../../../components/PageContainer/PageContainer';
 import { Link as RouterLink } from 'react-router-dom';
+import { PinDrop } from '@mui/icons-material';
 
 interface Props {
   location: string;
@@ -41,8 +39,9 @@ type Profiles = Profile[];
 export default function ProfileListing({}: Props): ReactElement {
   //currently fake profiles for state
   const [profiles, setProfiles] = useState<Profiles>([]);
+  const classes = useStyles();
 
-  console.log(profiles[0]);
+  console.log(profiles);
 
   // use this later when we can integrate
   useEffect(() => {
@@ -53,56 +52,85 @@ export default function ProfileListing({}: Props): ReactElement {
       userId: '61df3960442e349d92bad441',
       name: 'Roland Matheson',
       title: 'Pet Lover',
-      description: 'I love dogcats',
+      description:
+        'I love dogcats Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis assumenda omnis obcaecati, cumque, nisi animi quam voluptatum tempora enim illo at id iusto! Iure eveniet cum in a, accusamus earum!',
       address: 'Tokyo, Japan',
       pay: '$99/Hour',
       _id: '61df3960442e349d9200000',
       photo: 'fakeimage',
     };
-    const profiles = [profile];
-    setProfiles(profiles);
+    const fakeProfiles = [profile, profile, profile, profile, profile];
+    setProfiles(fakeProfiles);
   }, []);
 
   return (
     <PageContainer>
-      <Grid container>
-        <Typography gutterBottom variant="h2" component="div">
+      <Box>
+        <Typography className={classes.header} variant="h3" component="div">
           {`Your search results`}
         </Typography>
-        {profiles.length > 0 &&
-          profiles.map((profile) => {
-            <Grid key={profile.userId} item xs={3}>
-              <Card>
-                <CardActionArea component={RouterLink} target="_blank" to={`/profile${profile._id}`}>
-                  <CardMedia>
-                    {' '}
-                    <Avatar alt={`${profile.name} profile picture`} src={`someimageurl/${profile.photo}`} />{' '}
-                  </CardMedia>
 
-                  <CardContent>
-                    <Typography gutterBottom variant="h4" component="div">
-                      {`${profile.name}`}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {`${profile.title}`}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {`${profile.description}`}
-                    </Typography>
+        <Grid container spacing={2}>
+          {profiles.length > 0 &&
+            profiles.map((profile) => (
+              <Grid key={profile.userId} item xs={4}>
+                <Card className={classes.profileCard}>
+                  <CardActionArea component={RouterLink} target="_blank" to={`/profile${profile._id}`}>
+                    <CardMedia>
+                      {' '}
+                      <Avatar
+                        className={classes.profileAvatar}
+                        alt={`${profile.name} profile picture`}
+                        src={`someimageurl/${profile.photo}`}
+                      />{' '}
+                    </CardMedia>
 
-                    <Typography variant="body2" color="text.secondary">
-                      {`${profile.address}`}
-                    </Typography>
+                    <CardContent>
+                      <Typography className={classes.profileName} gutterBottom variant="h4" component="div">
+                        {`${profile.name}`}
+                      </Typography>
+                      <Typography
+                        className={classes.profileTitle}
+                        gutterBottom
+                        variant="h5"
+                        color="text.secondary"
+                        component="div"
+                      >
+                        {`${profile.title}`}
+                      </Typography>
 
-                    <Typography variant="body2" color="text.secondary">
-                      {`${profile.pay}`}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>;
-          })}
-      </Grid>
+                      <Rating name="half-rating" className={classes.profileRating} defaultValue={2.5} precision={0.5} />
+
+                      <Typography className={classes.profileDescription}>{`${profile.description}`}</Typography>
+                      <Divider />
+                      <Grid container spacing={2}>
+                        {' '}
+                        <Grid item xs={1}>
+                          <PinDrop />
+                        </Grid>
+                        <Grid item xs={4}>
+                          <Typography className={classes.profileAddress} color="text.secondary">
+                            {`${profile.address}`}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                          <Typography className={classes.profilePay} color="text.secondary">
+                            {`${profile.pay}`}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+
+        <Button className={classes.button} onClick={() => window.alert('I was clicked')}>
+          {' '}
+          Show more{' '}
+        </Button>
+      </Box>
     </PageContainer>
   );
 }
