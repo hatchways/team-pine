60995510c83774aed75b45cba8e9eb4995d69d68
Gurkirt Singh <GurkirtSingh143@gmail.com>
2022-01-20 00:13:34 -0800

    Created controllers for availability
    Following controllers are created for availability route
    1. created new schedule
    2. get schedule by id
    3. get list of all the schedule
    4. get active schedule
    5. set active schedule

---
+++ server/controllers/availability.js
@@ -0,0 +1,98 @@
+const asyncHandler = require("express-async-handler");
+const Profile = require("../models/Profile");
+const Schedule = require("../models/Schedule");
+
+exports.createSchedule = asyncHandler(async (req, res, next) => {
+  const profileId = req.profile.id;
+  const name = req.body.name;
+  const days = req.body.days;
+  try {
+    const newSchedule = new Schedule({
+      name,
+      profileId,
+      days,
+    });
+    const savedSchedule = await newSchedule.save();
+    if (!savedSchedule) {
+      res.status(500);
+      throw new Error("Unexpected Error: Unable to save new schedule");
+    }
+    res.status(201);
+    res.send("schedule is created");
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+});
+// @route GET /availability/:scheduleId
+// @desc Get Schedule by Id
+// @access Private
+exports.getSchedule = asyncHandler(async (req, res, next) => {
+  const scheduleId = req.params.scheduleId;
+  try {
+    const schedule = await Schedule.findOne({ _id: scheduleId })
+      .where("profileId")
+      .equals(req.profile.id);
+    if (schedule) {
+      res.status(200);
+      res.send(schedule);
+    } else {
+      res.status(404);
+      res.send("schedule is not found!!");
+    }
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+});
+// @route GET /availability
+// @desc Get list of all schedules
+// @access Private
+exports.getAvailability = asyncHandler(async (req, res, next) => {
+  const profile = req.profile;
+  try {
+    const schedules = await Schedule.find({ profileId: profile.id });
+    res.status(200);
+    res.send(schedules);
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+});
+
+// @route GET /availability/active
+// @desc Get active schedule
+// @access Private
+exports.getActiveSchedule = asyncHandler(async (req, res, next) => {
+  const profile = req.profile;
+  try {
+    const schedule = await Schedule.findOne({ _id: profile.activeSchedule });
+    res.status(200);
+    res.send(schedule);
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+});
+
+// @route PATCH /availability/:scheduleId/activate
+// @desc Set active schedule
+// @access Private
+exports.setActiveSchedule = asyncHandler(async (req, res, next) => {
+  const scheduleId = req.params.scheduleId;
+  try {
+    const schedule = await Schedule.findOne({ _id: scheduleId });
+    const profile = await Profile.findOne({ _id: req.profile.id });
+    profile.set({ activeSchedule: schedule.id });
+    const updatedProfile = await profile.save();
+    res.status(200);
+    res.send({
+      success: {
+        profile: updatedProfile,
+      },
+    });
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+});
