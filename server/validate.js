9d9e60a870d90ac29d7f484bfef7ad96b4403612
Gurkirt Singh <GurkirtSingh143@gmail.com>
2022-01-20 00:15:26 -0800

    Created validation function for new schedule request

--- server/validate.js
+++ server/validate.js
@@ -1,4 +1,5 @@
-const { check, validationResult } = require("express-validator");
+const { check, validationResult, body } = require("express-validator");
+const Schedule = require("./models/Schedule");
 
 exports.validateRegister = [
   check("name", "Please enter a name").not().isEmpty(),
@@ -7,7 +8,7 @@ exports.validateRegister = [
     "password",
     "Please enter a password with 6 or more characters"
   ).isLength({
-    min: 6
+    min: 6,
   }),
   (req, res, next) => {
     const errors = validationResult(req);
@@ -16,7 +17,7 @@ exports.validateRegister = [
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  }
+  },
 ];
 
 exports.validateLogin = [
@@ -28,5 +29,42 @@ exports.validateLogin = [
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
     next();
-  }
+  },
+];
+
+exports.validateScheduleSchema = [
+  body("name", "schedule name is required")
+    .not()
+    .isEmpty()
+    .isString()
+    .custom(async (name, { req }) => {
+      const schedule = await Schedule.findOne({
+        name,
+        profileId: req.profile.id,
+      });
+      if (schedule) {
+        return Promise.reject("Schedule name is already exists");
+      }
+    }),
+  body("days.*.isAvailable", "is available required")
+    .not()
+    .isEmpty()
+    .isBoolean()
+    .withMessage("only accepts true or false"),
+
+  body("days.*.start", "start hour is required")
+    .notEmpty()
+    .isInt({ min: 0, max: 23 })
+    .withMessage("start hour should be a number between 0 to 23"),
+  body("days.*.end", "end hour is required")
+    .notEmpty()
+    .isInt({ min: 0, max: 23 })
+    .withMessage("end hour should be a number between 0 to 23"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+    next();
+  },
 ];
