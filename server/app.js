2cf2ba74c918a5df60ca223b71268cc9e67ac21e
Roland <mathesonroland@gmail.com>
2022-01-22 15:19:33 +0900

    Revert "Notification Model, Controllers and Routes"

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ const logger = require("morgan");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const profileRouter = require('./routes/profile');
-const notificationRouter = require('./routes/notification');
 
 const { json, urlencoded } = express;
 
@@ -46,7 +45,6 @@ app.use((req, res, next) => {
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/profile", profileRouter);
-app.use("/notifications", notificationRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
