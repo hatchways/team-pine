deb57ff354b5599835153f88034c59fc805dd8ad
Gurkirt Singh <GurkirtSingh143@gmail.com>
2022-01-24 20:32:43 -0800

    changed validation error response structure

--- server/validate.js
+++ server/validate.js
@@ -66,7 +66,11 @@ exports.validateScheduleSchema = [
     const errors = validationResult(req);
 
     if (!errors.isEmpty())
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({
+        error: {
+          message: `${errors.array()[0].param} - ${errors.array()[0].msg}`,
+        },
+      });
     next();
   },
 ];
