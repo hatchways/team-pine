03bac03883f76fb197e48050d7c76169c93bb81a
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-02-05 00:49:22 -0500

    Validation
    Added validation middleware to the review creation route.

--- server/routes/review.js
+++ server/routes/review.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { validateReview } = require("../validate");
 const protect = require("../middleware/auth");
 const {
   createReview,
   getReviews,
 } = require("../controllers/review");
 
-router.route("/:profileId").post(protect, createReview);
+router.route("/:profileId").post(protect, validateReview, createReview);
 
 router.route("/:profileId").get(protect, getReviews);
 
--- server/validate.js
+++ server/validate.js
@@ -70,3 +70,15 @@ exports.validateScheduleSchema = [
     next();
   },
 ];
+
+exports.validateReview = [
+  body("rating", "Rating is required").not().isEmpty().isInt({ min: 1, max: 5 }).withMessage("Rating must be a number between 1 and 5"),
+  body("text", "Exceeded character limit").isLength({ max: 2000 }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+    next();
+  }
+]
