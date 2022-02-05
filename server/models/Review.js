d9400364641182d48d239589fa35c869563ff1d1
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-02-05 01:08:05 -0500

    Update Review.js
    Added some validation to the model

--- server/models/Review.js
+++ server/models/Review.js
@@ -8,12 +8,16 @@ const reviewSchema = new mongoose.Schema({
   },
   rating: {
     type: Number,
-    required: true,
+    required: [true, "Must include a rating"],
+    min: [1, "Rating must be between 1 and 5"],
+    max: [5, "Rating must be between 1 and 5"],
   },
   text: {
     type: String,
     default: "",
+    maxlength: [2000, "Review must be 2000 characters or less"],
   }
-});
+},
+{ timestamps: true });
 
 module.exports = Review = mongoose.model("Review", reviewSchema);
