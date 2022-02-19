05ab7ef3985fbda6e753a383a329909c3a0bd924
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-02-19 15:07:15 -0500

    Update AuthPageWrapper.tsx

--- client/src/components/AuthPageWrapper/AuthPageWrapper.tsx
+++ client/src/components/AuthPageWrapper/AuthPageWrapper.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Paper } from '@mui/material';
 import AuthPageHeader from '../AuthPageHeader/AuthPageHeader';
-import { useState, useEffect } from 'react';
 
 interface AuthPageWrapperProps {
   header: string;
