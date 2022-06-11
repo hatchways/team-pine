5ad20800c2f713f9604391c95cfc505104975e07
GurkirtSingh <GurkirtSingh143@gmail.com>
2022-06-11 00:17:59 -0700

    Fixed to serve project in production

--- server/app.js
+++ server/app.js
@@ -13,25 +13,41 @@ const logger = require("morgan");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const profileRouter = require("./routes/profile");
-const uploadRouter = require("./routes/upload");
-const deleteRouter = require("./routes/delete");
 const availabilityRouter = require("./routes/availability");
 const paymentsRoute = require("./routes/payment");
+const notificationRouter = require("./routes/notification");
+const conversationRouter = require("./routes/conversation");
+const requestRouter = require("./routes/request");
+const uploadRouter = require("./routes/upload");
+const deleteRouter = require("./routes/delete");
+const reviewRouter = require("./routes/review");
+const cors = require("cors");
 
 const { json, urlencoded } = express;
 
 connectDB();
+
 const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      console.log(origin);
+      if (process.env.ALLOWED_ORIGINS.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 const server = http.createServer(app);
 
 const io = socketio(server, {
   cors: {
     origin: "*",
   },
-});
-
-io.on("connection", (socket) => {
-  console.log("connected");
 });
 
 if (process.env.NODE_ENV === "development") {
@@ -54,17 +70,20 @@ app.use((req, res, next) => {
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/profile", profileRouter);
+app.use("/notifications", notificationRouter);
+app.use("/conversations", conversationRouter);
+app.use("/requests", requestRouter);
 app.use("/upload", uploadRouter);
 app.use("/delete", deleteRouter);
 app.use("/availability", availabilityRouter);
-app.use("/payments", paymentsRoute);
+app.use("/reviews", reviewRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname), "client", "build", "index.html")
-  );
+  app.use(express.static(path.join(__dirname, "/../client/build")));
+  app.get("*", (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, "/../client", "build", "index.html"));
+  });
+  server.listen(process.env.PORT);
 } else {
   app.get("/", (req, res) => {
     res.send("API is running");
